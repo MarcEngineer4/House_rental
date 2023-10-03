081b54af8e40bcef075d199cf505569c099274da
MarcEngineer4 <marcengineer4@gmail.com>
2023-10-04 00:25:08 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express= require("express");
+const app=express();
+
+app.use(express.static("public"))
+app.get("/", function (req,res) {
+    res.sendFile(__dirname + "/index.html");
+})
+app.get("/proprietes", function (req,res) {
+    res.sendFile(__dirname + "/proprietes.html");
+})
+app.get("/details", function (req,res) {
+    res.sendFile(__dirname + "/details.html");
+})
+app.listen(3000, function () {
+    console.log("Server is Up and running..")
+})
+
+
